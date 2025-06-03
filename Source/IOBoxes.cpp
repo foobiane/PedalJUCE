637330f7c8862a6833d47a0d5f1d66ce5f6b28c2
Ian Doherty <dohertyiane@outlook.com>
2025-06-03 14:27:18 -0500

    Added I/O logic and fixed some graphical bugs; need to debug connections to I/O boxes

---
+++ Source/IOBoxes.cpp
@@ -0,0 +1,107 @@
+#include "IOBoxes.h"
+
+#include <iostream> // debug
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// InputBox
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+InputBox::InputBox(juce::AudioProcessorGraph* graph, int w, int h) : juce::AudioProcessorGraph::AudioGraphIOProcessor(juce::AudioProcessorGraph::AudioGraphIOProcessor::audioInputNode) {
+    g = graph;
+    width = w;
+    height = h;
+
+    numChannels = 2; // TODO: Make this detect the number of audio channels automatically
+    setPlayConfigDetails(0, numChannels, DEFAULT_SAMPLE_RATE, DEFAULT_BLOCK_SIZE);
+
+    initializePorts();
+}
+
+void InputBox::initializePorts() {
+    for (int i = 0; i < numChannels; i++) {
+        juce::Point<int> pos = juce::Point<int>(0, (int)((i + 1) * (height / (numChannels + 1)))) + getPosition();
+        ports.push_back(new Connector(g, juce::AudioProcessorGraph::NodeID(0), i, pos));
+    }
+}
+
+void InputBox::updatePorts() {
+    for (int i = 0; i < numChannels; i++) {
+        juce::Point<int> pos = juce::Point<int>(0, (int)((i + 1) * (height / (numChannels + 1)))) + getPosition();
+        ports[i]->updateStartPoint(pos);
+    }
+}
+
+void InputBox::paint(juce::Graphics& g) {
+    // TODO: Add labels
+    juce::Rectangle<int> area = getBounds();
+
+    g.setColour(juce::Colour(0.0f, 0.0f, 0.75f, 1.0f));
+    g.fillRoundedRectangle(0, 0, area.getWidth(), area.getHeight(), 20);
+
+    area.setSize(0.75 * width, height);
+
+    g.setColour(juce::Colour(0.0f, 0.0f, 0.70f, 1.0f));
+    g.fillRoundedRectangle(0.25 * width, 0, area.getWidth(), area.getHeight(), 20);
+}
+
+void InputBox::resized() {
+    updatePorts();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// OutputBox
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+OutputBox::OutputBox(juce::AudioProcessorGraph* graph, int w, int h) : juce::AudioProcessorGraph::AudioGraphIOProcessor(juce::AudioProcessorGraph::AudioGraphIOProcessor::audioOutputNode) {
+    g = graph;
+    width = w;
+    height = h;
+
+    numChannels = 2;
+    setPlayConfigDetails(numChannels, 0, DEFAULT_SAMPLE_RATE, DEFAULT_BLOCK_SIZE);
+
+    initializePorts();
+}
+
+void OutputBox::initializePorts() {
+    for (int i = 0; i < numChannels; i++) {
+        juce::Point<int> pos = juce::Point<int>(width, (int)((i + 1) * (height / (numChannels + 1)))) + getPosition();
+        juce::Rectangle<int> area(pos.translated(-1 * MAX_CONNECTION_RANGE, -1 * MAX_CONNECTION_RANGE), pos.translated(MAX_CONNECTION_RANGE, MAX_CONNECTION_RANGE));
+
+        InputPort* port = new InputPort();
+        port->setBounds(area);
+
+        ports.push_back(port);
+    }
+}
+
+void OutputBox::updatePorts() {
+    for (int i = 0; i < numChannels; i++) {
+        juce::Point<int> pos = juce::Point<int>(width, (int)((i + 1) * (height / (numChannels + 1)))) + getPosition();
+        juce::Rectangle<int> oldBounds = ports[i]->getBounds();
+
+        ports[i]->setBounds(pos.x - MAX_CONNECTION_RANGE, pos.y - MAX_CONNECTION_RANGE, oldBounds.getWidth(), oldBounds.getHeight());
+
+        // If our input port is connected to another connector, update that connector too
+        Connector* incoming = ports[i]->getIncomingConnector();
+        if (incoming != nullptr)
+            incoming->updateEndPoint(pos);
+    }
+}
+
+void OutputBox::paint(juce::Graphics& g) {
+    // TODO: Add labels
+    juce::Rectangle<int> area = getBounds();
+
+    g.setColour(juce::Colour(0.0f, 0.0f, 0.75f, 1.0f));
+    g.fillRoundedRectangle(0, 0, area.getWidth(), area.getHeight(), 20);
+
+    area.setSize(0.75 * width, height);
+
+    g.setColour(juce::Colour(0.0f, 0.0f, 0.70f, 1.0f));
+    g.fillRoundedRectangle(0, 0, area.getWidth(), area.getHeight(), 20);
+}
+
+void OutputBox::resized() {
+    updatePorts();
+}
---
+++ Source/IOBoxes.h
@@ -0,0 +1,64 @@
+#include <JuceHeader.h>
+
+#include "Pedal.h"
+
+const juce::AudioProcessorGraph::NodeID INPUT_BOX_NODE_ID = juce::AudioProcessorGraph::NodeID(0);
+const juce::AudioProcessorGraph::NodeID OUTPUT_BOX_NODE_ID = juce::AudioProcessorGraph::NodeID(1);
+
+class InputBox : public juce::AudioProcessorGraph::AudioGraphIOProcessor, public juce::Component {
+    public:
+        InputBox(juce::AudioProcessorGraph* graph, int w, int h);
+
+        bool isInput() { return true; }
+        bool isOutput() { return false; }
+
+        int getNumChannels() { return numChannels; }
+        void setWidth(int newWidth) { width = newWidth; }
+        int getWidth() { return width; }
+        void setHeight(int newHeight) { height = newHeight; }
+        int getHeight() { return height; }
+        
+        void paint(juce::Graphics& g) override;
+        void resized() override;
+
+        std::vector<Connector*> ports;
+
+    private:
+        int width, height;
+        int numChannels;
+
+        void initializePorts();
+        void updatePorts();
+
+        juce::AudioProcessorGraph* g;
+};
+
+class OutputBox : public juce::AudioProcessorGraph::AudioGraphIOProcessor, public juce::Component {
+    public:
+        OutputBox(juce::AudioProcessorGraph* graph, int w, int h);
+
+        bool isInput() { return false; }
+        bool isOutput() { return true; }
+        
+        int getNumChannels() { return numChannels; }
+        void setWidth(int newWidth) { width = newWidth; }
+        int getWidth() { return width; }
+        void setHeight(int newHeight) { height = newHeight; }
+        int getHeight() { return height; }
+
+        juce::Point<int> getPositionOfPort(int channel) { return ports[channel]->getPosition().translated(MAX_CONNECTION_RANGE, MAX_CONNECTION_RANGE); }
+
+        void paint(juce::Graphics& g) override;
+        void resized() override;
+
+        std::vector<InputPort*> ports;
+
+    private:
+        int width, height;
+        int numChannels;
+        
+        void initializePorts();
+        void updatePorts();
+
+        juce::AudioProcessorGraph* g;
+};
